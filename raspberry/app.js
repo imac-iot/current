cce5031408e2d302d57ddb2246d2f45456b77eeb
TitanLi <Lisheng0706@gmail.com>
2017-08-03 09:28:32 +0800

    Add raspberry#3

--- raspberry/app.js
+++ raspberry/app.js
@@ -2,18 +2,18 @@ var SerialPort = require("serialport");   //10.28.120.28
 var mqtt    = require('mqtt');            //192.168.8.126
 var client  = mqtt.connect('mqtt://10.28.120.28:1883');
 
-// var port = new SerialPort("/dev/ttyACM1", {
-//     parser: SerialPort.parsers.readline('\n')
-// });
-//
+var port = new SerialPort("/dev/ttyACM1", {
+    parser: SerialPort.parsers.readline('\n')
+});
+
 client.on('connect', function () {
   console.log('on connect');
   client.subscribe('current');
 });
-//
-// port.on('open', function() {
-//     port.on('data', function(data) {
-//         console.log(data);
-//         client.publish('current', data.toString());
-//     });
-// });
+
+port.on('open', function() {
+    port.on('data', function(data) {
+        console.log(data);
+        client.publish('current', data.toString());
+    });
+});
